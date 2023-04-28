96f82129ad8125a0a1f0ba645532c10ee5ed2ee6
Arpit Kumar Singh <singharpit302@gmail.com>
2023-04-28 20:11:11 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,39 @@ import Main from './Main'
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying Metamask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccount()
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if(networkData) {
+      const decetragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+    } else {
+      window.alert('Decentragram contract not deployed to detected network')
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -26,7 +59,6 @@ class App extends Component {
             // Code...
             />
           }
-        }
       </div>
     );
   }
